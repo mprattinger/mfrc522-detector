affdde77978a3e026f9fa0c13d8ecdecc341d4c5
Michael Prattinger <Michael Prattinger>
2017-02-01 05:36:37 +0100

    winchecker

--- winChecker.js
+++ winChecker.js
@@ -10,11 +10,18 @@ class WinChecker extends events.EventEmitter{
     init(){
         this.mockFile = "mockFile.json";
         if(!fs.existsSync(this.mockFile)){
-            
+            var data = JSON.stringify(new MockData());
+            fs.writeFileSync(this.mockFile, data);
         }
     }
 
     checkCard(){
         fs.watchFile();
+    }
+}
+
+class MockData{
+    constructor(){
+        this.card = false;
     }
 }
